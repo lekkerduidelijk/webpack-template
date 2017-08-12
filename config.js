7dcfa0eb9aec4c3fba76ebf9ba1e517525287a25
Rutger Laurman <info@lekkerduidelijk.nl>
2017-08-12 14:55:16 +0200

    WIP: webpack config

---
+++ config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const { argv } = require('yargs');
+const merge = require('webpack-merge');
+
+const userConfig = require('./src/assets/config');
+
+const isProduction = !!((argv.env && argv.env.production) || argv.p);
+const rootPath = (userConfig.paths && userConfig.paths.root)
+  ? userConfig.paths.root
+  : process.cwd();
+
+const config = merge({
+  open: true,
+  copy: 'images/**/*',
+  proxyUrl: 'http://localhost:3000',
+  cacheBusting: '[name]_[hash]',
+  paths: {
+    root: rootPath,
+    assets: path.join(rootPath, 'src/assets'),
+    dist: path.join(rootPath, 'dist'),
+  },
+  enabled: {
+    sourceMaps: !isProduction,
+    optimize: isProduction,
+    cacheBusting: isProduction,
+    watcher: !!argv.watch,
+  },
+  watch: [],
+}, userConfig);
+
+module.exports = merge(config, {
+  env: Object.assign({ production: isProduction, development: !isProduction }, argv.env),
+  publicPath: `${config.publicPath}/${path.basename(config.paths.dist)}/`,
+  manifest: {},
+});
+
+if (process.env.NODE_ENV === undefined) {
+  process.env.NODE_ENV = isProduction ? 'production' : 'development';
+}
